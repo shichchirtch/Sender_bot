1826c2011a6af45c01156d3aa88d2b609f6a14ab
shichchirtch <ivanvol9l@gmail.com>
2024-12-16 23:20:20 +0100

    3 Commit
    убрана кнопка старт

--- bot/start_menu.py
+++ bot/start_menu.py
@@ -3,8 +3,6 @@ from aiogram.types import BotCommand
 
 async def set_main_menu(bot):
     main_menu_commands = [
-        BotCommand(command='/start',
-                   description='Go to START'),
 
         BotCommand(command='/help',
                    description='go to personal chatting')
