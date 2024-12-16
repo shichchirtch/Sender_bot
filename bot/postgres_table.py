b164e146efb062615bc684393df9de26317d4561
shichchirtch <ivanvol9l@gmail.com>
2024-12-16 13:23:13 +0100

    2 Commit
    Add new Admin

--- bot/postgres_table.py
+++ bot/postgres_table.py
@@ -27,6 +27,6 @@ class User(Base):
 
 async def init_models():
     async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
+        # await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
 
